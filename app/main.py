c10e0e28ba33fe4bc84de9d3b50c9bdf8d1814ec
sen zhan <sen.zhan@outlook.com>
2026-01-08 20:58:25 +0100

    add name

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def index() -> str:
-    return "Hello, Welcome to my store!"
+    return "Hello, Welcome to my store, Sen Zhan!"
 
 @app.get("/health")
 def health_check() -> dict:
